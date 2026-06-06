736b1907259389c5627be59f19dc77da9a14cc7c
Mikael Rousson <mikael.rousson@gmail.com>
2026-06-06 15:32:17 +0800

    Add security headers and app manifest

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,32 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ out: 'build' })
+		adapter: adapter({ out: 'build' }),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['self'],
+				'base-uri': ['none'],
+				'connect-src': ['self', ...(isDevelopment ? ['ws:'] : [])],
+				'font-src': ['self', 'https://fonts.gstatic.com'],
+				'form-action': ['self'],
+				'frame-ancestors': ['none'],
+				'frame-src': ['self', 'blob:', 'https:'],
+				'img-src': ['self', 'blob:', 'data:', 'https:'],
+				'manifest-src': ['self'],
+				'media-src': ['self', 'blob:', 'https:'],
+				'object-src': ['none'],
+				'script-src': ['self'],
+				'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+				'worker-src': ['self', 'blob:'],
+				'upgrade-insecure-requests': true
+			}
+		}
 	}
 };
 
